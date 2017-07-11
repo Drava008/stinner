c2d08bbbe869dca27c01273057cfeed92a909b5b
Drava008 <ymps9869111@gmail.com>
2017-07-11 11:27:37 +0800

    新增選取功能

--- app/src/main/java/com/example/user/stinner/MainActivity.java
+++ app/src/main/java/com/example/user/stinner/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.user.stinner;
 
+import android.support.v4.content.SharedPreferencesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.Checkable;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Spinner spinner;
     String[] friut = new String []{"香蕉","蘋果","鳳梨"};
     TextView textView;
+    CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +26,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView)findViewById(R.id.textView);
-
+        checkBox = (CheckBox)findViewById(R.id.checkBox);
         spinner = (Spinner)findViewById(R.id.spinner);
+
         ArrayAdapter<String> adapterFruit = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,friut);
         adapterFruit.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapterFruit);
 
+        //監聽選取文字
         spinner.setOnItemSelectedListener( new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -39,6 +47,22 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        //選取功能
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (checkBox.isChecked()){
+                    Toast.makeText(MainActivity.this,"選取",Toast.LENGTH_SHORT).show();
+                    checkBox.setText("取消選取");
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"取消選取",Toast.LENGTH_SHORT).show();
+                    checkBox.setText("選取");
+                }
+            }
+        });
+
 
     }
 }
